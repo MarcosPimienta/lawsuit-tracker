a6e1e428c500f11670d30aab352bd5407f6914c6
MarcosPimienta <fenix3819@gmail.com>
2026-06-19 11:37:38 -0500

    Fixed typos errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the process list heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/lista de procesos/i)).toBeInTheDocument();
 });
